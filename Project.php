d774d3959ccf52684e68ffad9a739fc72e1bd60b
abdokhaled <abdokhaled23334@gmail.com>
2025-07-03 22:44:05 +0300

    add addToCart function

--- Project.php
+++ Project.php
@@ -3,30 +3,30 @@
 abstract class Category
 {
 
-    abstract function __construct(array $items);
-    abstract function show();
-}
-
-class ClothesCategory extends Category
-{
-
     public function __construct(public array $items) {}
 
     public function show()
     {
-        foreach ($this->items as $category => $items) {
+        foreach ($this->items as $category => $product) {
             echo $category . ":\n";
-            foreach ($items as $item) {
-                echo "-" . $item . "\n";
+            foreach ($product as $item) {
+
+                echo "-" . $item["name"] . " => " . $item["price"]  . "\n";
             }
         }
+        echo "-------------------------------------";
     }
 }
 
 
 
+class ClothesCategory extends Category {}
+
 class Amazon
 {
+
+    private $x = 1;
+    private $totalPrice = 0 ;
 
     public function __construct(public array $categories) {}
     public function showCategories()
@@ -34,6 +34,7 @@ class Amazon
         foreach ($this->categories as $categories) {
             echo $categories . "\n";
         }
+        echo "------------------------------------- \n";
     }
 
     public function selectCategory(Category $category)
@@ -41,7 +42,15 @@ class Amazon
         $category->show();
     }
 
-    public function addToCart() {}
+    public function addToCart(array $items) {
+        foreach ($items as $item){
+            echo ($this->x === 1 ) ? "Added " : false ;
+            $this->x++;
+            echo $item["name"] . ", " ; 
+            $this->totalPrice += $item['price'];
+        }
+        echo "To Cart. \nThe Total Price = {$this->totalPrice}" ;
+    }
 
     public function buyItems() {}
 }
@@ -58,19 +67,56 @@ $amazon = new Amazon([
 $clothesCategory = new ClothesCategory(
     [
         "Men" => [
-            "Jacket",
-            "T-Shirt",
-            "Trousers",
-            "Shoes"
+            [
+                "name" => "Jacket",
+                "price" => 120
+            ],
+            [
+                "name" => "T-Shirt",
+                "price" => 100
+            ],
+
+            [
+                "name" => "Trousers",
+                "price" => 150
+            ],
+            [
+                "name" => "Shoes",
+                "price" => 80
+            ]
         ],
 
         "Women" => [
-            "Dress",
-            "skirt",
-            "boots",
-            "Shoes"
+            [
+                "name" => "Dress",
+                "price" => 2000
+            ],
+            [
+                "name" => "Coat",
+                "price" => 1400
+            ],
+
+            [
+                "name" => "Trousers",
+                "price" => 850
+            ],
+            [
+                "name" => "Shoes",
+                "price" => 1000
+            ]
         ]
     ]
 );
 
-$amazon->selectCategory($clothesCategory);
+
+// Show Categories items in amazon
+// $amazon->showCategories();
+// select  one Category to show items 
+// $amazon->selectCategory($clothesCategory);
+//add the item to cart
+$amazon->addToCart([
+    $clothesCategory->items["Men"][0],
+    $clothesCategory->items["Men"][1],
+    $clothesCategory->items["Women"][3]
+]); 
+//pay the money
